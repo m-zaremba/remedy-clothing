5d5a9069d2ce5ccc59f6e0fcf1b54ea52f6a5a2b
m-zaremba <zaremba_michal@wp.pl>
2020-04-16 13:48:40 +0200

    fetch database with redux-saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,9 +1,20 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+import { fetchCollectionSuccess, fetchCollectionsFailure } from './shop.actions';
 
 import shopActionTypes from './shop.types';
 
 export function* fetchCollectionsAsync() {
-  yield console.log('I am fired');
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot);
+    yield put(fetchCollectionSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
 }
 
 export function* fetchCollectionsStart() {
