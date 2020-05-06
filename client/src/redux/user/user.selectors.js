e8650af736365fde4d476688bc00daef6f9b9537
m-zaremba <zaremba_michal@wp.pl>
2020-05-06 16:28:22 +0200

    swapped redux with context (step 1)

--- client/src/redux/user/user.selectors.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-import { createSelector } from 'reselect';
-
-const selectUser = (state) => state.user;
-
-export const selectCurrentUser = createSelector([selectUser], (user) => user.currentUser);
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,54 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+
+import CurrentUserContext from "../../contexts/current-user/current-user.context";
+import { CartContext } from "../../providers/cart/cart.provider";
+
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import "./header.styles.scss";
+
+const Header = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const { hidden } = useContext(CartContext);
+
+  return (
+    <div className="header">
+      <Link className="logo-container" to="/">
+        <Logo className="logo" />
+      </Link>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )}
+          <CartIcon />
+      </div>
+      {hidden ? null : <CartDropdown />}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  hidden: selectCartHidden,
+});
+
+export default connect(mapStateToProps)(Header);
