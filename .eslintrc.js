5cf45f42e3afb7aa218583b01d44fb79c9d11f21
m-zaremba <zaremba_michal@wp.pl>
2020-05-09 18:40:21 +0200

    added poetic package

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
+  // Add custom rules here
+  rules: {
+    'react/prop-types': 0,
+    'no-console': 0,
+    'no-alert': 0,
+  },
+};
