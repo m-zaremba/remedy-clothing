f59e02cf48eb80c35b67b355bbd7c654ac459f38
m-zaremba <zaremba_michal@wp.pl>
2020-05-09 18:23:00 +0200

    stripe publishable key change and hide

--- src/components/spinner/spinner.component.jsx
+++ src/components/spinner/spinner.component.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import './spinner.styles.scss';
+import "./spinner.styles.scss";
 
 const Spinner = () => (
-  <div className='spinner-overlay'>
-    <div className='spinner-container' />
+  <div className="spinner-overlay">
+    <div className="spinner-container" />
   </div>
 );
 
