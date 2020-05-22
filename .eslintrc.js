0cb978d0c8d3e1875589b7a938517bd4f9364646
m-zaremba <zaremba_michal@wp.pl>
2020-05-22 06:28:36 +0200

    eslint rules update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,9 @@ module.exports = {
     'react/prop-types': 0,
     'no-console': 0,
     'no-alert': 0,
+    'import/prefer-default-export': 0,
+    'import/no-named-default': 0,
+    'no-shadow': 0,
+    'react/jsx-filename-extension': 0,
   },
 };
