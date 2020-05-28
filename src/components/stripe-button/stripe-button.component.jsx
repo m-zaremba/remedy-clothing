2b240dbdb1b7d3f2fd746af255adefe9d4783289
m-zaremba <zaremba_michal@wp.pl>
2020-05-28 19:59:41 +0200

    new stripe publishable key hidden in .env file

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
 
-  const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!');
+  const onToken = (token) => {
+    alert("Payment Successful!");
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label="Pay Now"
+      name="Remedy Clth"
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image="https://sendeyo.com/up/d/f3eb2117da"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
