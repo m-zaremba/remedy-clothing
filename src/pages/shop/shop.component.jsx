836b75cdfb0f3a3a66950171513c5bf851e7c74a
m-zaremba <zaremba_michal@wp.pl>
2020-06-03 16:46:00 +0200

    data fetch from firestore update

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -10,31 +10,31 @@ import { CollectionsContext } from "../../providers/collections/collections.prov
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
-
 import WithLoader from "../../components/collections-loader/collections-loader.component";
 
 const CollectionsOverviewWithLoader = WithLoader(CollectionsOverview);
 const CollectionPageWithLoader = WithLoader(CollectionPage);
 
 const ShopPage = ({ match }) => {
-  const { setContextCollections } = useContext(CollectionsContext);
-  const [fetchedCollections, setFetchedCollections] = useState(null);
+  const { collections, setContextCollections } = useContext(CollectionsContext);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = firestore
-      .collection("collections")
-      .onSnapshot(async (snapshot) => {
-        const convertedCollections = convertCollectionsSnapshotToMap(snapshot);
-        setFetchedCollections(convertedCollections);
-        setIsLoading(false);
-      });
-    return () => unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    setContextCollections(fetchedCollections);
-  }, [fetchedCollections, setContextCollections]);
+    if (collections === null) {
+      const unsubscribe = firestore
+        .collection("collections")
+        .onSnapshot(async (snapshot) => {
+          const convertedCollections = convertCollectionsSnapshotToMap(
+            snapshot
+          );
+          setContextCollections(convertedCollections);
+          setIsLoading(false);
+        });
+      return () => unsubscribe();
+    } else {
+      setIsLoading(false);
+    }
+  }, [collections, setContextCollections]);
 
   return (
     <div className="shop-page">
